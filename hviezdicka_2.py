d080bdd17ba0df3c32bf7607d052734e7a51d619
KJUB0 <jakubferencik3@gmail.com>
2024-10-08 20:36:18 +0200

    ready na poslanie, spraveny def3()

--- hviezdicka_2.py
+++ hviezdicka_2.py
@@ -10,13 +10,22 @@ pocitadlo = vyska
 if vyska % 2 == 0:
     vyska += 1
 
-for riadok in range(0, vyska):       
-    for i in range(0, vyska):
-        print(hviezdicka if (riadok == vyska/2-0.5 or riadok == i or i == vyska/2-0.5 or i == pocitadlo) else(' '), end=' ')
-    pocitadlo -= 1      
-    print()
-    
-    
+def verzia3():
+    hviezdicka = "*"
+    #vyska = int(input( "Vyska hviezdicky?" ))
+    vyska = 6
+    pocitadlo = vyska
+
+    if vyska % 2 == 0:
+        vyska += 1
+        
+    for riadok in range(0, vyska):       
+        for i in range(0, vyska):
+            print(hviezdicka if (riadok == vyska/2-0.5 or riadok == i or i == vyska/2-0.5 or i == pocitadlo) else(' '), end=' ')
+        pocitadlo -= 1      
+        print()
+
+verzia3()
 end = time.perf_counter()
 
 print(end - start)
