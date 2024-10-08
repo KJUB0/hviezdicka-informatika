de7b822652b7003e2b43e469515564dae533adb5
KJUB0 <jakubferencik3@gmail.com>
2024-10-08 20:08:54 +0200

    este jednoduchsie

--- hviezdicka_2.py
+++ hviezdicka_2.py
@@ -10,20 +10,12 @@ pocitadlo = vyska
 if vyska % 2 == 0:
     vyska += 1
 
-for riadok in range(0, vyska):
-    if riadok == vyska/2-0.5:
-        for i in range(0, vyska):
-            print(hviezdicka, end=" ")
-    
-        pocitadlo -= 1    
-    
-        
-    else:
-        for i in range(0, vyska):
-            print(hviezdicka if (riadok == i or i == vyska/2-0.5 or i == pocitadlo) else(' '), end=' ')
-        pocitadlo -= 1      
-    
+for riadok in range(0, vyska):       
+    for i in range(0, vyska):
+        print(hviezdicka if (riadok == vyska/2-0.5 or riadok == i or i == vyska/2-0.5 or i == pocitadlo) else(' '), end=' ')
+    pocitadlo -= 1      
     print()
+    
     
 end = time.perf_counter()
 
